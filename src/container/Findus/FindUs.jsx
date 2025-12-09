e0d94a8eb1f4488d46630434aab13d9c228e501b
emenike-ozioma <emenikeozioma227@gmail.com>
2025-12-09 14:23:12 +0100

    findus section

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -8,7 +8,7 @@ const FindUs = () => (
             <SubHeading title={"Contact"} />
             <h1 className="headtext__cormorant" style={{marginBottom: "3rem"}}>Find Us</h1>
             <div className="app__wrapper-content">
-                <p className="p__opensans">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque, recusandae.</p>
+                <p className="p__opensans">Lan ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG</p>
                 <p className="p__cormorant" style={{color: "#DCCA87", margin: "2rem 0"}}>Opening hours</p>
                 <p className="p__opensans">Mon - Fri: 10:00am - 02:00am</p>
                 <p className="p__opensans">Sat - Sun: 10:00am - 03:00am</p>
